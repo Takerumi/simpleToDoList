9a903153a29c5b80263b6cecd8d8055e7313b1e8
Borman <mrgabrielripper@gmail.com>
2022-05-09 15:21:13 +0600

    Create sceleton for this project

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express'),
+  app = express(),
+  port = process.env.PORT || 3000
+
+let items = ['Buy bread', 'Buy cheese', 'By sausages']
+
+app.set('view engine', 'ejs')
+
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+  const today = new Date(),
+    options = {
+      weekday: 'long',
+      day: 'numeric',
+      month: 'long',
+    }
+  let day = today.toLocaleDateString('ru-RU', options)
+
+  res.render('list', { day: day, items: items })
+})
+
+app.post('/', (req, res) => {
+  let item = req.body.newItem
+  items.push(item)
+  res.redirect('/')
+})
+
+app.listen(port, () => console.log(`Server is running on port ${port}`))
