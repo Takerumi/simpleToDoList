b6efa4e1b27c54197ad6c812997b5bf9aae75cf8
Borman <mrgabrielripper@gmail.com>
2022-05-15 14:33:29 +0600

    Connect app to local DB

--- app.js
+++ app.js
@@ -1,16 +1,44 @@
+const { name } = require('ejs')
+
 const express = require('express'),
   app = express(),
   port = process.env.PORT || 3000,
-  date = require(`${__dirname}/date.js`)
+  date = require(`${__dirname}/date.js`),
+  mongoose = require('mongoose')
 
-let items = [],
-  workItems = []
+require('dotenv').config()
+
+const DB_HOST = process.env.DB_HOST
 
 app.set('view engine', 'ejs')
 
 app.use(express.urlencoded({ extended: true }))
-
 app.use(express.static('public'))
+
+mongoose.connect(DB_HOST)
+
+const itemSchema = new mongoose.Schema({
+    name: String,
+  }),
+  Item = mongoose.model('Item', itemSchema),
+  item1 = new Item({
+    name: 'Welcome to your todoList!',
+  }),
+  item2 = new Item({
+    name: 'Hit the + button to add a new item',
+  }),
+  item3 = new Item({
+    name: '<-- Hit this to delete an item',
+  }),
+  defaultItems = [item1, item2, item3]
+
+Item.insertMany(defaultItems, (err) => {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log('Successfully add items')
+  }
+})
 
 app.get('/', (req, res) => {
   let day = date.getDate()
