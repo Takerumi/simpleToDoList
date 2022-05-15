97793beccf7b9834cdc7783dc8782592107f672b
Borman <mrgabrielripper@gmail.com>
2022-05-15 18:54:07 +0600

    Add ability to create and delete entries

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const { name } = require('ejs')
+const { redirect } = require('express/lib/response')
 
 const express = require('express'),
   app = express(),
@@ -32,29 +33,33 @@ const itemSchema = new mongoose.Schema({
   }),
   defaultItems = [item1, item2, item3]
 
-Item.insertMany(defaultItems, (err) => {
-  if (err) {
-    console.log(err)
-  } else {
-    console.log('Successfully add items')
-  }
-})
-
 app.get('/', (req, res) => {
-  let day = date.getDate()
-  res.render('list', { listTitle: day, items: items })
+  Item.find({}, (err, items) => {
+    if (items.length === 0) {
+      Item.insertMany(defaultItems, (err) => {
+        if (err) {
+          console.log(err)
+        } else {
+          console.log('Successfully add items')
+        }
+      })
+      res.redirect('/')
+    } else {
+      let day = date.getDate()
+      res.render('list', { listTitle: day, items: items })
+    }
+  })
 })
 
 app.post('/', (req, res) => {
-  let item = req.body.newItem
+  const itemName = req.body.newItem
 
-  if (req.body.list === 'Work') {
-    workItems.push(item)
-    res.redirect('/work')
-  } else {
-    items.push(item)
-    res.redirect('/')
-  }
+  const item = new Item({
+    name: itemName,
+  })
+
+  item.save()
+  res.redirect('/')
 })
 
 app.get('/work', (req, res) => {
@@ -65,6 +70,21 @@ app.post('/work', (req, res) => {
   let item = req.body.newItem
   workItems.push(item)
   res.redirect('/work')
+})
+
+app.post('/delete', (req, res) => {
+  const checkedItemId = req.body.checkbox
+  function deleteItem(id) {
+    Item.findByIdAndRemove(id, (err) => {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log('Successfully deleted checked item from DB')
+      }
+    })
+  }
+  deleteItem(checkedItemId)
+  res.redirect('/')
 })
 
 app.get('/about', (req, res) => {
